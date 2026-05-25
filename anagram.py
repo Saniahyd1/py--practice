1253709c33e5753044e50d98704ff0c08fa6792d
saniahyd1 <saniahyd1@gmail.com>
2026-05-25 08:44:59 +0530

    anagram file is added

---
+++ anagram.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        count = {}
+        
+        for char in s:
+            count[char] = count.get(char, 0) + 1
+        
+        for char in t:
+            if char not in count or count[char] == 0:
+                return False
+            count[char] -= 1
+        
+        return True
+    
+s= input("Enter the first string: ")
+t= input("Enter the second string: ")
+solution = Solution()
+print(solution.isAnagram(s, t))
