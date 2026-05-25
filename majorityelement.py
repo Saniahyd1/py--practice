39232fdb3ab5804f6860c576341a2455374a5d0f
saniahyd1 <saniahyd1@gmail.com>
2026-05-25 08:50:27 +0530

    majority element file is added

---
+++ majorityelement.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        majority = nums[0]
+        vote = 1
+        for i in range(1, len(nums)):
+            if vote == 0:
+                majority = nums[i]
+                vote = 1
+            elif nums[i] == majority:
+                vote += 1
+            else:
+                vote -= 1
+        return majority
+nums = list(map(int, input("Enter the list of numbers: ").split()))
+sol = Solution()  
+print(sol.majorityElement(nums))
