685a1c3e396082cb1a7e0dfeecce2e1cecf52cad
saniahyd1 <saniahyd1@gmail.com>
2026-06-14 07:29:08 +0530

    3sum is edited

--- threesum.py
+++ threesum.py
@@ -1,10 +1,12 @@
 class Solution:
     def threeSum(self, nums:list[int])->list[list[int]]:
-        result= set()
-        for i in range(len(nums)-2):
-            for j in range(i+1, len(nums)-1):
+        result= set() 
+
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
                 for k in range(j+1, len(nums)):
-                    if nums[i]+nums[k]+nums[j]==0:
+
+                    if nums[i] + nums[k] + nums[j] == 0:
                         triplet = tuple(sorted([nums[i], nums[j], nums[k]]))
                         result.add(triplet)
         return [list(t) for t in result]
