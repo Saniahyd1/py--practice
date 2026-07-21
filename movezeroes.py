556e51a9393aafd1ab7efa1a2cb511e5478afe5c
saniahyd1 <saniahyd1@gmail.com>
2026-07-21 06:49:44 +0530

    modified2

--- movezeroes.py
+++ movezeroes.py
@@ -1,14 +1,11 @@
 def moveZeroes(nums):
     j = 0  # position for next non-zero element
-
     for i in range(len(nums)):
         if nums[i] != 0:
             # swap only if needed
             if i != j:
                 nums[j], nums[i] = nums[i], nums[j]
             j += 1
-
-
 # ---- Driver Code ----
 if __name__ == "__main__":
     nums = list(map(int, input("Enter numbers separated by space: ").split()))
