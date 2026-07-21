5402e8a90d0f26429b9237e9b3241731c499bf50
saniahyd1 <saniahyd1@gmail.com>
2026-07-21 06:47:57 +0530

    modified

--- threesum.py
+++ threesum.py
@@ -1,16 +1,13 @@
 class Solution:
     def threeSum(self, nums:list[int])->list[list[int]]:
         result= set() 
-
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
                 for k in range(j+1, len(nums)):
-
                     if nums[i] + nums[k] + nums[j] == 0:
                         triplet = tuple(sorted([nums[i], nums[j], nums[k]]))
                         result.add(triplet)
         return [list(t) for t in result]
-
 if __name__ == "__main__":
     nums = input("Enter the array elements separated by spaces: ").split()
     nums = [int(x) for x in nums if x.strip()]
