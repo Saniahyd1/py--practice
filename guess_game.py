7585e14da2b8b46ffdbe242aec2bbe2b4b608c23
saniahyd1 <saniahyd1@gmail.com>
2026-09-13 22:59:49 +0530

    modified3

--- guess_game.py
+++ guess_game.py
@@ -1,15 +1,11 @@
 import random
-
 print(" Number Guessing Game")
 print("I am thinking of a number between 1 and 100")
-
 number = random.randint(1, 100)
 attempts = 0
-
 while True:
     guess = int(input("\nEnter your guess: "))
     attempts += 1
-
     if guess < number:
         print("Too low!")
     elif guess > number:
