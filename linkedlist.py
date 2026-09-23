f1af6cb8b8ab7c302f2d0bd57a2a75a7bb764198
saniahyd1 <saniahyd1@gmail.com>
2026-09-23 06:49:24 +0530

    modified

--- linkedlist.py
+++ linkedlist.py
@@ -5,7 +5,6 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
-
     def append(self, data):
         new_node = Node(data)
         if not self.head:
@@ -15,46 +14,36 @@ class LinkedList:
         while last_node.next:
             last_node = last_node.next
         last_node.next = new_node
-
     def print_list(self):
         current_node = self.head
         while current_node:
             print(current_node.data, end=' ')
             current_node = current_node.next
         print()
-# ---- Driver Code ----
 if __name__ == "__main__":
     linked_list = LinkedList()
     elements = list(map(int, input("Enter numbers separated by space: ").split()))
-    
     for elem in elements:
         linked_list.append(elem)
-    
     print("Linked List:", end=' ')
     linked_list.print_list()
 #--delete the node at a given position--
 def delete_node(head, position):
     if head is None:
         return head
-
     temp = head
-
     if position == 0:
         head = temp.next
         temp = None
         return head
-
     for i in range(position - 1):
         temp = temp.next
         if temp is None:
             break
-
     if temp is None or temp.next is None:
         return head
-
     next = temp.next.next
     temp.next = None
     temp.next = next
-
     return head
 #---delete
